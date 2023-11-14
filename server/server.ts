d87c589f122b0ad50992b953f3e2f26e16890fa1
Asteris Giannoudis <asteris.gian@gmail.com>
2023-11-14 12:31:29 +0200

    Simple NodeJS server and Vite React client.

---
+++ server/server.ts
@@ -0,0 +1,27 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+// Serve the React static files after build
+app.use(express.static("../client/build"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on ${PORT}`);
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.send({ message: "Hello" });
+});
+
+// All other unmatched requests will return the React app
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server running");
+});
