79a07e28fa8743daff842fc13b4337cbebc8ad09
Asteris Giannoudis <asteris.gian@gmail.com>
2023-11-15 16:59:43 +0200

    [ SERVICE ] Created service for handling progress CSV files.

--- server/controllers/progress.ts
+++ server/controllers/progress.ts
@@ -1,49 +1,13 @@
 import { RequestHandler } from "express";
-import path from "path";
-import { parse, stringify } from "csv/sync";
-import { promises as fs } from "fs";
-
-const DATA_FOLDER = process.env.DATA_FOLDER || "data";
+import { getCSVData } from "../services/csv-service";
 
 // GET /api/progress/draft
 export const getProgressDraft: RequestHandler = async (req, res, next) => {
-  const week = req.query.week;
-  const day = req.query.day;
+  const week = req.query.week?.toString();
+  const day = req.query.day?.toString();
 
   try {
-    let content;
-    // [?week=XX&day=YY]: If both params present on the URL return the requested progress
-    if (week && day) {
-      content = await fs.readFile(
-        path.resolve(
-          __dirname,
-          "..",
-          "..",
-          DATA_FOLDER,
-          `week${week}`,
-          "progress",
-          `progress.draft.w${week}.d${day}.csv`
-        )
-      );
-    }
-    // TODO: [?week=XX]: If only param week is present return progress for all days
-    // If no params present on the URL return the first day of the first week
-    else {
-      // console.log(process.env.DATA_FOLDER + __dirname
-      content = await fs.readFile(
-        path.resolve(
-          __dirname,
-          "..",
-          "..",
-          DATA_FOLDER,
-          "week01",
-          "progress",
-          "progress.draft.w01.d01.csv"
-        )
-      );
-    }
-
-    const records = content && parse(content);
+    const records = await getCSVData("draft.", week, day);
     res.status(200).json({ records });
   } catch (error) {
     console.log(error);
---
+++ server/services/csv-service.ts
@@ -0,0 +1,63 @@
+import path from "path";
+import { parse, stringify } from "csv/sync";
+import { promises as fs } from "fs";
+
+const DATA_FOLDER = process.env.DATA_FOLDER || "data";
+
+/**
+ * @param week: string
+ * @param day?: string
+ *
+ * @returns array of JSON objects { columnName: value } from CSV file(s) with first row as header.
+ */
+export const getCSVData = async (
+  type: string,
+  week?: string,
+  day?: string
+) => {
+  if (day) {
+    return parse(
+      await fs.readFile(
+        path.resolve(
+          __dirname,
+          "..",
+          "..",
+          DATA_FOLDER,
+          `week${week}`,
+          "progress",
+          `progress.${type}w${week}.d${day}.csv`
+        )
+      ),
+      {
+        columns: true,
+      }
+    );
+  } else if (week) {
+    let records: any = [];
+    const days = ["01", "02", "03", "04", "05"];
+
+    for (const d of days) {
+      records.push(
+        parse(
+          await fs.readFile(
+            path.resolve(
+              __dirname,
+              "..",
+              "..",
+              DATA_FOLDER,
+              `week${week}`,
+              "progress",
+              `progress.${type}w${week}.d${d}.csv`
+            )
+          ),
+          {
+            columns: true,
+          }
+        )
+      );
+    }
+    return [].concat(...records);
+  } else {
+    throw new Error("Week parameter is required!");
+  }
+};
