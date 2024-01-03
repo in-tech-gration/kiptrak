1736aa0f1bfabaec8e3995249941f30c0a3d1846
Kostas Minaidis <minaidisk@gmail.com>
2024-01-03 19:50:05 +0200

    [ CLIENT ][ RTK ] Implemented RTK Post Crud hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,60 @@
 import "./App.css";
-import { useGetProgressByWeekQuery } from './services/progress'
+import { 
+  useGetProgressByWeekQuery,
+  usePostWeeklyProgressMutation 
+} from './services/progress'
 import Spreadsheet from "./components/spreasheet";
 
 function App() {
 
+  // WiP: C[R]UD
   const { data, error, isLoading } = useGetProgressByWeekQuery({
     week:'01',
     day: '02'
   });
 
+  // WiP: [C]RUD
+  const [ addWeek ] = usePostWeeklyProgressMutation();
+  const postData = {
+    "week": "01",
+    "day": "01",
+    "data": [
+      {
+        "week": 1,
+        "day": 1,
+        "concept": "Setting up: Markdown",
+        "task": "Watch the 'What is markdown?' video",
+        "level": "Beginner",
+        "confidence": 7,
+        "completed": true,
+        "instructions": "Update CONFIDENCE column. Change COMPLETED column to TRUE "
+      },
+      {
+        "week": 1,
+        "day": 1,
+        "concept": "Setting up: CSV",
+        "task": "Complete the 'Understanding CSV Files' challenge",
+        "level": "Beginner",
+        "confidence": 8,
+        "completed": true,
+        "instructions": "Update CONFIDENCE column. Change COMPLETED column to TRUE "
+      }
+    ]
+  }
+  const handleAddWeek = async ()=>{
+    try {
+      const response = await addWeek(postData).unwrap();
+      console.log({ response });
+      
+    } catch(e){
+      console.error("handleAddWeek()",e);
+    }
+  }
+
   return (
     <>
       <h1>KipTrak Client</h1>
+      <button onClick={handleAddWeek}>Post week</button>
       {error ? (
         <>Oh no, there was an error</>
       ) : isLoading ? (
--- client/src/services/progress.ts
+++ client/src/services/progress.ts
@@ -8,6 +8,7 @@ export const progressApi = createApi({
   reducerPath: 'progressApi',
   baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
   endpoints: (builder) => ({
+    // GET PROGRESS BY WEEK:
     getProgressByWeek: builder.query({
     // TODO: Assign Type to: getPokemonByName: builder.query<Pokemon, string>({
       query: ({ week, day }) => {
@@ -15,9 +16,19 @@ export const progressApi = createApi({
         return `?week=${week}&day=${day}`
       }
     }),
+    // POST WEEKLY PROGRESS:
+    postWeeklyProgress: builder.mutation({
+      query: (week)=>{
+        return {
+          url: "",
+          method: "POST",
+          body: week
+        }
+      }
+    })
   }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetProgressByWeekQuery } = progressApi
+export const { useGetProgressByWeekQuery, usePostWeeklyProgressMutation } = progressApi
